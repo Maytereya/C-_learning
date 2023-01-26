71e504f52f31feb45ca882a09fd45e3d6c7ee0e0
Vladimir <rakhmanov-v@ya.ru>
2023-01-26 11:47:27 +0400

    Размещены примеры на суммирование и деление

---
+++ Example003_SUM/Program.cs
@@ -0,0 +1,9 @@
+﻿
+    
+        Console.WriteLine("Сложение 2х чисел");
+        Console.WriteLine("введите число А"); 
+        NumberA = int.Parse(Console.ReadLine());
+        Console.WriteLine("введите число B");
+        NumberB = int.Parse(Console.ReadLine());
+        Console.Write(NumberA + NumberB);
+        Console.Write(" - сумма чисел");

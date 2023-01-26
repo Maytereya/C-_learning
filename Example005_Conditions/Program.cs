ab83f8311c6d509230781e58be5ebf6f3f7aeeb0
Vladimir <rakhmanov-v@ya.ru>
2023-01-26 12:39:20 +0400

    Добавили оператор ToLower()

---
+++ Example005_Conditions/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+ {
+    Console.WriteLine ("Ура, это же Masha!");
+ }
+
+else
+{
+Console.WriteLine("Hello ");
+Console.WriteLine(username);
+
+}
